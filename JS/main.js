af04e769db8320df56befcfe4d6e2e8136e2ba4f
Malczewski94 <tonnymalczewski01@gmail.com>
2023-04-24 21:52:38 -0300

    primra preentrega 2

--- JS/main.js
+++ JS/main.js
@@ -1,32 +1,34 @@
-// Valores ingresados por el usuario
+// Obtener valores del formulario
 function calcularInteresCompuesto() {
-    let capital = parseFloat(document.getElementById("capital").value);
-    let tasa = parseFloat(document.getElementById("tasa").value);
-    let tiempo = parseInt(document.getElementById("tiempo").value);
-// Verificar que los valores ingresados sean válidos
-    if (isNaN(capital) || isNaN(tasa) || isNaN(tiempo)) {
-        alert("Por favor, ingrese valores numéricos válidos.");
-        return;
-    }
-    let monto = calcularMontoTotal(capital, tasa, tiempo);
-    let interes = calcularInteresTotal(monto, capital);
-    document.getElementById("resultado-monto").innerHTML = "Monto total: $" + monto.toFixed(2);
-    document.getElementById("resultado-interes").innerHTML = "Interés total: $" + interes.toFixed(2);
+	let capitalInicial = parseFloat(document.getElementById("capitalInicial").value);
+	let tasaInteres = parseFloat(document.getElementById("tasaInteres").value) / 100;
+	let tiempoInversion = parseFloat(document.getElementById("tiempoInversion").value);
+	let capitalMensual = parseFloat(document.getElementById("capitalMensual").value);
+// Calcular interés compuesto
+	let capitalFinal = capitalInicial;
+	for (let i = 1; i <= tiempoInversion * 12; i++) {
+		let interes = capitalFinal * (tasaInteres / 12);
+		capitalFinal += interes;
+		capitalFinal += capitalMensual;
+	}
+// Validar valores
+	if (isNaN(capitalInicial) || isNaN(tasaInteres) || isNaN(tiempoInversion) || isNaN(capitalMensual)) {
+		alert("Por favor ingrese valores numéricos para todos los campos.");
+		return;
+	}
+	if (capitalInicial <= 0 || tasaInteres <= 0 || tiempoInversion <= 0 || capitalMensual < 0) {
+		alert("Los valores ingresados deben ser mayores que cero.");
+		return;
+	}
+// Mostrar resultado
+	let resultadoDiv = document.getElementById("resultado");
+	resultadoDiv.style.display = "block";
+	resultadoDiv.innerHTML = "<h2>Resultado:</h2><p>Capital final: $" + capitalFinal.toFixed(2) + "</p>";
 }
 
-// Calcular el interés compuesto
-function calcularMontoTotal(capital, tasa, tiempo) {
-    return capital * Math.pow(1 + (tasa / 100), tiempo);
-}
-
-function calcularInteresTotal(monto, capital) {
-    return monto - capital;
-}
-
+// Reiniciar formulario y resultado
 function reiniciarSimulador() {
-    document.getElementById("capital").value = "";
-    document.getElementById("tasa").value = "";
-    document.getElementById("tiempo").value = "";
-    document.getElementById("resultado-monto").innerHTML = "";
-    document.getElementById("resultado-interes").innerHTML = "";
+	document.getElementById("formulario").reset();
+	document.getElementById("resultado").style.display = "none";
 }
+
